097a4be27e15cd3ad82e626d89ec754c98bde36c
Herrius <65200014+Herrius@users.noreply.github.com>
2022-04-19 20:39:42 -0500

    criptomonedas dogecoin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import logo from './resource/img/cryptocurrencies.png';
+import PriceCard from './components/pricecard';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [ticker, setTicker] = useState({
+    low: 0,
+    high: 0,
+    last: 0,
+  });
+
+  useEffect(() => {
+    async function getDogecoinPrice() {
+      const { data } = await axios.get(
+        'https://nitinr-cors.herokuapp.com/https://api.wazirx.com/api/v2/tickers/dogeusdt'
+      );
+      setTicker(data.ticker);
+    }
+    getDogecoinPrice();
+    const interval = setInterval(() => getDogecoinPrice(), 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <img src={logo} width={150} height={150} alt="cryptomoney"></img>
+      <h1 className='title'>Criptocurrency Ticket</h1>
+      <p className='subtitle'>Criptocurrency price currently 🌐</p>
+      <div className="prices-container">
+        <PriceCard type="low" price={ticker.low} />
+        <PriceCard type="high" price={ticker.high} />
+        <PriceCard type="current" price={ticker.last} />
+      </div>
     </div>
   );
 }
